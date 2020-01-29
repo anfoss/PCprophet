aa5f6ba481de8cd7208c0b924388e304f8f1c719
Andrea Fossati <anfossat@biol-sysbc-hpx-docking-1-021.ethz.ch>
2020-01-29 10:33:13 +0100

    [FIX] port to networkX 2.4 required

--- PCprophet/go_fdr.py
+++ PCprophet/go_fdr.py
@@ -17,7 +17,7 @@ def db2ppi(list_sep):
     for members in list_sep:
         for pairs in st.fast_comb(members.split("#"), 2):
             ppi_db.add_edge(str.upper(pairs[0]), str.upper(pairs[1]))
-    # ppi_db.remove_edges_from(nx.selfloop_edges())
+    ppi_db.remove_edges_from(nx.selfloop_edges(ppi_db, keys=True))
     return ppi_db
 
 
