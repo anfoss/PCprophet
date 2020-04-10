7252a005c384b12cf8c1c9a718b03742096b12a2
Andrea Fossati <anfossat@IMSB-IT-MBP12.local>
2020-04-10 09:55:53 +0200

    [FIX] setup

--- setup.py
+++ setup.py
@@ -3,45 +3,69 @@
 from setuptools import setup, find_packages
 
 
-setup(
-    name="PCProphet",
-    version="1.0.dev",
+long_description = 'PCprophet is a complete toolset for analyzing coelution mass spectrometry data, by prediction of novel complexes, identification of reported complexes and differential analysis across conditions'
+
+setup_args = dict(
+    name='PCProphet',
+    version='0.0.1',
     packages=find_packages(),
     scripts=[
-        "collapse.py",
-        "differential.py",
-        "exceptions.py",
-        "generate_features.py",
-        "io_.py",
-        "hypothesis.py",
-        "mcl.py",
-        "map_to_database.py",
-        "parse_GO.py",
-        "runner.py",
-        #'plot_sec.py',
-        "signal_prc.py",
-        "stats_.py",
-        "validate_input.py",
+        'collapse.py',
+        'differential.py',
+        'exceptions.py',
+        'generate_features.py',
+        'go_fdr.py',
+        'io_.py',
+        'hypothesis.py',
+        'mcl.py',
+        'init.py'
+        'map_to_database.py',
+        'mcl.py',
+        'merge.py',
+        'parse_GO.py',
+        'main.py',
+        'plots.py',
+        'predict.py',
+        'stats_.py',
+        'validate_input.py',
     ],
     # long_description=long_description,
-    license="GNU General Public License v3.0",
+    license='BSD',
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=["scipy>=1.1", "pandas", "sklearn", "networkX"],
+    install_requires=['scipy>=1.1', 'pandas', 'sklearn', 'networkX'],
     package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        "": ["*.txt", "*.obo"],
+        'PCprophet': ['go_term_class.txt', 'go-basic.obo', 'rf_equal.clf'],
     },
     # metadata to display on PyPI
-    author="Andrea Fossati",
-    author_email="fossati@imsb.biol.ethz.ch",
-    description="Prophet for analysis of co-fractionation data",
-    keywords="Machine Learning Protein Complexes MS Fractionation",
-    url="https://github.com/ChompOrDie/PCProphet",
+    author='Andrea Fossati',
+    author_email='fossati@imsb.biol.ethz.ch',
+    description='Software toolset for analysis of co-fractionation data',
+    keywords=['proteomics', 'machine-learning', 'signal-processing'],
+    url='https://github.com/fossatiA/PCProphet/',
     project_urls={
-        "Bug Tracker": "https://github.com/fossatiA/PCProphet/",
-        "Documentation": "https://github.com/fossatiA/PCProphet/",
-        "Source Code": "https://github.com/fossatiA/PCProphet/",
+        'Bug Tracker': 'https://github.com/fossatiA/PCProphet/',
+        'Documentation': 'https://github.com/fossatiA/PCProphet/',
+        'Source Code': 'https://github.com/fossatiA/PCProphet/',
     },
-    classifiers=["License :: OSI Approved :: Python Software Foundation License"],
+    platforms="Linux, Mac OS X, Windows",
+    long_description=long_description,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Modified BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
 )
+# TODO add compilation for Java GUI
+
+
+def main():
+    setup(**setup_args)
+
+
+if __name__ == '__main__':
+    main()
